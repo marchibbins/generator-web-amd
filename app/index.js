be5478436f5a0b9982f81e3608dd4ab3f9e60d26
Marc Hibbins <marchibbins@gmail.com>
2016-08-04 11:57:53 +0100

    Index template

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+    writing: function () {
+        this.directory(
+            this.templatePath(),
+            this.destinationPath()
+        );
+    }
+});
