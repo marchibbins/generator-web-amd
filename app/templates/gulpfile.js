ab73fdb1f682d59e335306e8f74c60d3dc401551
Marc Hibbins <marchibbins@gmail.com>
2016-08-05 21:36:20 +0100

    Add uglify

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp'),
-    requirejsOptimize = require('gulp-requirejs-optimize');
+    requirejsOptimize = require('gulp-requirejs-optimize'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 
 gulp.task('scripts', function () {
     return gulp.src('scripts/main.js')
         .pipe(requirejsOptimize({
             include: ['almond', 'main']
         }))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
 });
 
